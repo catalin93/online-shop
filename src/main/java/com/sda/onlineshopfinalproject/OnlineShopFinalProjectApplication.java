56a355b7b23a3e987936a22ad0ace4b483707988
Chirila Catalin <chirilacatalin93@gmail.com>
2023-03-20 20:37:05 +0200

    first comit

---
+++ src/main/java/com/sda/onlineshopfinalproject/OnlineShopFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sda.onlineshopfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sda/onlineshopfinalproject/OnlineShopFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.onlineshopfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
