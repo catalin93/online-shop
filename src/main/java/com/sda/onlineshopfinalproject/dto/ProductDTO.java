c5cd41188ecd402b544da9e705f6cb365cb3d1f0
Chirila Catalin <chirilacatalin93@gmail.com>
2023-03-23 21:07:25 +0200

    Implemented home page

--- src/main/java/com/sda/onlineshopfinalproject/dto/ProductDTO.java
+++ src/main/java/com/sda/onlineshopfinalproject/dto/ProductDTO.java
@@ -1,9 +1,16 @@
 package com.sda.onlineshopfinalproject.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDTO {
+    private String id;
     private String name;
     private String description;
     private String price;
